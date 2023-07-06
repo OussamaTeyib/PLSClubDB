5ef4d70924d6c4b795330efe08575eab23266ab7
Oussama Teyib <oussama.teyib@gmail.com>
2023-07-06 16:50:39 +0000

    Added support to wide chars

--- src/main.c
+++ src/main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <wchar.h>
 #include <windows.h>
 
-void die(char *msg)
+void die(wchar_t *msg)
 {
-    fprintf(stderr, "%s\n", msg);
+    fwprintf(stderr, L"%ls\n", msg);
     exit(EXIT_FAILURE);
 }
 
@@ -15,38 +16,39 @@ void die(char *msg)
 typedef struct {
     int id;
     int isKnown;
-    char fakeName[MAX];
-    char realName[MAX];
+    wchar_t fakeName[MAX];
+    wchar_t realName[MAX];
 } Club;
 
 int main(void)
 {
     int choice;
+    system("chcp 1252");
     system("cls");
-    printf("What do you want:\n");
-    printf("	1. Add a league\n");
-    printf("	2. Modify a league\n");
-    printf("	3. List a league\n");
-    printf("	4. Remove a league\n");
-    printf("	5. List all leagues\n");
-    printf("	0. Exit\n");
+    wprintf(L"What do you want:\n");
+    wprintf(L"	1. Add a league\n");
+    wprintf(L"	2. Modify a league\n");
+    wprintf(L"	3. List a league\n");
+    wprintf(L"	4. Remove a league\n");
+    wprintf(L"	5. List all leagues\n");
+    wprintf(L"	0. Exit\n");
 
-    printf("\nEnter your choice: ");
+    wprintf(L"\nEnter your choice: ");
     do
     {
         scanf("%d", &choice);
         if (choice < 0 || choice > 5)
-            printf("\nEnter a valid choice [0, 5]: ");
+            wprintf(L"\nEnter a valid choice [0, 5]: ");
     } while (choice < 0 || choice > 5);
     
-    char temp[MAX_LEAGUE_NAME], name[MAX_LEAGUE_NAME];
+    wchar_t temp[MAX_LEAGUE_NAME], name[MAX_LEAGUE_NAME];
     if (choice && choice != 5)
     {
-        printf("Enter the name of the league: ");
+        wprintf(L"Enter the name of the league: ");
         fflush(stdin);
-        fgets(temp, MAX_LEAGUE_NAME, stdin);
-        temp[strlen(temp) - 1] = '\0';
-        snprintf(name, MAX_LEAGUE_NAME, "../leagues/%s.bin", temp);
+        fgetws(temp, MAX_LEAGUE_NAME, stdin);
+        temp[wcslen(temp) - 1] = L'\0';
+        snwprintf(name, MAX_LEAGUE_NAME, L"../leagues/%ls.bin", temp);
     }
 
     int firstTime = 1;
@@ -55,19 +57,19 @@ int main(void)
         if (!firstTime)
         {
             system("cls");
-            printf("What do you want again:\n");
-            printf("	1. Modify the league\n");
-            printf("	2. List the league\n");
-            printf("	3. Remove the league\n");
-            printf("	0. Exit\n");
+            wprintf(L"What do you want again:\n");
+            wprintf(L"	1. Modify the league\n");
+            wprintf(L"	2. List the league\n");
+            wprintf(L"	3. Remove the league\n");
+            wprintf(L"	0. Exit\n");
 
-            printf("\nEnter your choice: ");
+            wprintf(L"\nEnter your choice: ");
             do
             {
                 fflush(stdin);
                 scanf("%d", &choice);
                 if (choice < 0 || choice > 3)
-                    printf("\nEnter a valid choice [0, 3]: ");
+                    wprintf(L"\nEnter a valid choice [0, 3]: ");
             } while (choice < 0 || choice > 3);
             
             if (choice)
@@ -82,19 +84,19 @@ int main(void)
         {
             case 1: // adding
             {
-                league = fopen(name, "wb");
+                league = _wfopen(name, L"wb");
                 if (!league)
-                    die("Cannot create the corresponding file!");
+                    die(L"Cannot create the corresponding file!");
                 
-                printf("\nEnter the number of clubs: ");
+                wprintf(L"\nEnter the number of clubs: ");
                 do
                 {              
                     fflush(stdin);
                     scanf("%d", &nClubs);
                     if (!nClubs)
                     {
-                        printf("\nNumber of clubs can't be null!\n");
-                        printf("Enter a valid number: ");
+                        wprintf(L"\nNumber of clubs can't be null!\n");
+                        wprintf(L"Enter a valid number: ");
                     }
                 } while (!nClubs);
                 fwrite(&nClubs, sizeof nClubs, 1, league);
@@ -103,27 +105,27 @@ int main(void)
                 while (counter < nClubs)
                 {
                     club.id = counter + 1;
-                    printf("\nClub #%d:\n", club.id);
-                    printf ("The real name is known? (1/0): ");
+                    wprintf(L"\nClub #%d:\n", club.id);
+                    wprintf(L"The real name is known? (1/0): ");
                     fflush(stdin);
                     scanf("%d", &club.isKnown);
 
                     if (club.isKnown)
                     {
-                        printf("Enter the real name: ");
+                        wprintf(L"Enter the real name: ");
                         fflush(stdin);
-                        fgets(club.realName, MAX, stdin);
-                        club.realName[strlen(club.realName) - 1] = '\0';
+                        fgetws(club.realName, MAX, stdin);
+                        club.realName[wcslen(club.realName) - 1] = L'\0';
 
-                        printf("Enter the fake name: ");
+                        wprintf(L"Enter the fake name: ");
                         fflush(stdin);
-                        fgets(club.fakeName, MAX, stdin);
-                        club.fakeName[strlen(club.fakeName) - 1] = '\0';
+                        fgetws(club.fakeName, MAX, stdin);
+                        club.fakeName[wcslen(club.fakeName) - 1] = L'\0';
                     }
                     else
                     {
-                        strncpy(club.fakeName, "Not Given", MAX);
-                        strncpy(club.realName, "Unknown", MAX);
+                        wcsncpy(club.fakeName, L"Not Given", MAX);
+                        wcsncpy(club.realName, L"Unknown", MAX);
                     }    
                     
                     fwrite(&club, sizeof club, 1, league);
@@ -137,15 +139,15 @@ int main(void)
             
             case 2: // modifying
             {
-                league = fopen(name, "rb+");
+                league = _wfopen(name, L"rb+");
                 if (!league)
-                    die("Cannot open the corresponding file!");
+                    die(L"Cannot open the corresponding file!");
                 
                 if (1 != fread(&nClubs, sizeof nClubs, 1, league))
-                    die("The corresponding file is empty!"); 
+                    die(L"The corresponding file is empty!"); 
                 
                 int tempID, found = 0;
-                printf ("Enter the id of the club: ");
+                wprintf(L"Enter the id of the club: ");
                 fflush(stdin);
                 scanf("%d", &tempID);
                
@@ -154,77 +156,77 @@ int main(void)
                 if (1 == fread(&club, sizeof club, 1, league) && club.id == tempID)
                 {
                     found = 1;
-                    printf("\nClub #%d\n", club.id);
-                    printf("Real Name: %s\n", club.realName);
-                    printf("Fake Name: %s\n", club.fakeName);
+                    wprintf(L"\nClub #%d\n", club.id);
+                    wprintf(L"Real Name: %ls\n", club.realName);
+                    wprintf(L"Fake Name: %ls\n", club.fakeName);
                         
                     int tempChoice;
-                    printf("\nWhat do you want:\n");
-                    printf("	1. Modify the fake Name\n");
-                    printf("	2. Modify the real Name\n");
-                    printf("	3. Modify them both\n");
-                    printf("	4. Set as unknown\n");
-                    printf("	0. Nothing\n");
-                    printf("Enter your choice: ");
+                    wprintf(L"\nWhat do you want:\n");
+                    wprintf(L"	1. Modify the fake Name\n");
+                    wprintf(L"	2. Modify the real Name\n");
+                    wprintf(L"	3. Modify them both\n");
+                    wprintf(L"	4. Set as unknown\n");
+                    wprintf(L"	0. Nothing\n");
+                    wprintf(L"Enter your choice: ");
                     do
                     {
                         fflush(stdin);
                         scanf("%d", &tempChoice);
                         if (tempChoice < 0 || tempChoice > 4)
-                            printf("\nEnter a valid choice: ");
+                            wprintf(L"\nEnter a valid choice: ");
                     } while (tempChoice < 0 || tempChoice > 4); 
                        
                     switch(tempChoice)
                     {
                         case 1: // modify the fake name
-                            printf("\nEnter the fake name: ");                           
+                            wprintf(L"\nEnter the fake name: ");                           
                             fflush(stdin);
-                            fgets(club.fakeName, MAX, stdin);
-                            club.fakeName[strlen(club.fakeName) - 1] = '\0';
+                            fgetws(club.fakeName, MAX, stdin);
+                            club.fakeName[wcslen(club.fakeName) - 1] = L'\0';
                             break;
                             
                         case 2: // modify the real name
-                            printf("\nEnter the real name: ");
+                            wprintf(L"\nEnter the real name: ");
                             fflush(stdin);
-                            fgets(club.realName, MAX, stdin);
-                            club.realName[strlen(club.realName) - 1] = '\0';
+                            fgetws(club.realName, MAX, stdin);
+                            club.realName[wcslen(club.realName) - 1] = L'\0';
                             club.isKnown = 1;
                             break;
                             
                         case 3: // modify them both
-                            printf("\nEnter the real name: ");
+                            wprintf(L"\nEnter the real name: ");
                             fflush(stdin);
-                            fgets(club.realName, MAX, stdin);
-                            club.realName[strlen(club.realName) - 1] = '\0';
+                            fgetws(club.realName, MAX, stdin);
+                            club.realName[wcslen(club.realName) - 1] = L'\0';
 
-                            printf("Enter the fake name: ");
+                            wprintf(L"Enter the fake name: ");
                             fflush(stdin);
-                            fgets(club.fakeName, MAX, stdin);
-                            club.fakeName[strlen(club.fakeName) - 1] = '\0';
+                            fgetws(club.fakeName, MAX, stdin);
+                            club.fakeName[wcslen(club.fakeName) - 1] = L'\0';
       
                             club.isKnown = 1;
                             break;
                
                         case 4: // set as an unknown club
-                            strncpy(club.fakeName, "Not Given", MAX);
-                            strncpy(club.realName, "Unknown", MAX);
+                            wcsncpy(club.fakeName, L"Not Given", MAX);
+                            wcsncpy(club.realName, L"Unknown", MAX);
                             club.isKnown = 0;
                             break;
 
                         case 0:
-                            printf("\nBe sure the next time!");
+                            wprintf(L"\nBe sure the next time!");
                     }
                         
                     if (tempChoice) 
                     {
                         fseek(league, - (long) sizeof club, SEEK_CUR);
                         fwrite(&club, sizeof club, 1, league);
-                        printf("\nDone!");
+                        wprintf(L"\nDone!");
                     }
                 }
                 
                 if (!found)
-                    printf("The ID is incorrect!\n");         
+                    wprintf(L"The ID is incorrect!\n");         
                 
                 fclose(league);    
                 repeat = 1;
@@ -233,16 +235,16 @@ int main(void)
 
             case 3: // print a league
             {
-                league = fopen(name, "rb");
+                league = _wfopen(name, L"rb");
                 if (!league)
-                    die("Cannot open the corresponding file!");
+                    die(L"Cannot open the corresponding file!");
                 
                 if (1 != fread(&nClubs, sizeof nClubs, 1, league))
-                    die("The corresponding file is empty!");
+                    die(L"The corresponding file is empty!");
 
                 int printUnknown;
-                printf("\nNumber of clubs is: %d\n", nClubs);
-                printf("Print unknown clubs? (1/0): ");
+                wprintf(L"\nNumber of clubs is: %d\n", nClubs);
+                wprintf(L"Print unknown clubs? (1/0): ");
                 fflush(stdin);
                 scanf("%d", &printUnknown);                                         
                 
@@ -251,9 +253,9 @@ int main(void)
                     if (!printUnknown && !club.isKnown)
                         continue;
                         
-                    printf("\nClub #%d\n", club.id);
-                    printf("Real Name: %s\n", club.realName);
-                    printf("Fake Name: %s\n", club.fakeName);
+                    wprintf(L"\nClub #%d\n", club.id);
+                    wprintf(L"Real Name: %ls\n", club.realName);
+                    wprintf(L"Fake Name: %ls\n", club.fakeName);
                 }
  
                 fclose(league);
@@ -264,23 +266,23 @@ int main(void)
             case 4: // removing
             {
                 int tempChoice;
-                printf("\nDo you want to remove the league? (1/0): ");
+                wprintf(L"\nDo you want to remove the league? (1/0): ");
                 fflush(stdin);
                 scanf("%d", &tempChoice);
                 if (tempChoice)
                 {
-                    if(!remove(name))
+                    if(!_wremove(name))
                     {
-                        printf("Removed!\n");
+                        wprintf(L"Removed!\n");
                         repeat = 0;    
                         again = 0;    
                     }
                     else
-                        die("Cannot find the file!");
+                        die(L"Cannot find the file!");
                 }
                 else
                 {
-                    printf("\nBe sure the next time!");
+                    wprintf(L"\nBe sure the next time!");
                     repeat = 1;
                 }
                 break;
@@ -288,13 +290,13 @@ int main(void)
 
             case 5: // print all leagues
             {  
-                WIN32_FIND_DATAA findData;
-                HANDLE hFind = FindFirstFileA("../leagues/*.bin", &findData);   
+                WIN32_FIND_DATAW findData;
+                HANDLE hFind = FindFirstFileW(L"../leagues/*.bin", &findData);   
                 if (hFind == INVALID_HANDLE_VALUE)
-                    die("Cannot find any corresponding file!");
+                    die(L"Cannot find any corresponding file!");
 
                 int printUnknown;
-                printf("Print unknown clubs? (1/0): ");
+                wprintf(L"Print unknown clubs? (1/0): ");
                 fflush(stdin);
                 scanf("%d", &printUnknown);
 
@@ -302,36 +304,36 @@ int main(void)
                 {
                     if (!(findData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY))
                     {
-                        char name[MAX_LEAGUE_NAME];                     
-                        snprintf(name, MAX_LEAGUE_NAME, "../leagues/%s", findData.cFileName);
-                        league = fopen(name, "rb");
+                        wchar_t name[MAX_LEAGUE_NAME];                     
+                        snwprintf(name, MAX_LEAGUE_NAME, L"../leagues/%ls", findData.cFileName);
+                        league = _wfopen(name, L"rb");
                         if (!league)
                         {
-                            fprintf(stderr, "ERROR: Cannot open '%s'!\n", findData.cFileName);
+                            fwprintf(stderr, L"ERROR: Cannot open '%ls'!\n", findData.cFileName);
                             continue;
                         }
                 
                         if (1 != fread(&nClubs, sizeof nClubs, 1, league))
                         {
-                            fprintf(stderr, "ERROR: '%s' is empty!\n", findData.cFileName);
+                            fwprintf(stderr, L"ERROR: '%ls' is empty!\n", findData.cFileName);
                             continue;
                         }
 
-                        printf("\nLeague: %s (%d clubs)\n", strtok(findData.cFileName, "."), nClubs);                                
+                        wprintf(L"\nLeague: %ls (%d clubs)\n", wcstok(findData.cFileName, L"."), nClubs);                                
                 
                         while (1 == fread(&club, sizeof club, 1, league))
                         {
                             if (!printUnknown && !club.isKnown)
                                 continue;
                         
-                            printf("\n	Club #%d\n", club.id);
-                            printf("	Real Name: %s\n", club.realName);
-                            printf("	Fake Name: %s\n", club.fakeName);
+                            wprintf(L"\n	Club #%d\n", club.id);
+                            wprintf(L"	Real Name: %ls\n", club.realName);
+                            wprintf(L"	Fake Name: %ls\n", club.fakeName);
                         }
-                        printf("\n");
+                        wprintf(L"\n");
                         fclose(league);
                     }
-                } while(FindNextFileA(hFind, &findData));
+                } while(FindNextFileW(hFind, &findData));
 
                 FindClose(hFind);
                 again = 0;
@@ -345,14 +347,14 @@ int main(void)
         }   
         if (repeat)
         {
-            printf("\nAgain? (1/0): ");
+            wprintf(L"\nAgain? (1/0): ");
             fflush(stdin);
             scanf("%d", &again);
         }
 
         if (!again)
         {
-            printf("\nGood Bye!");
+            wprintf(L"\nGood Bye!");
             return EXIT_SUCCESS;
         }
     }
